903c22638daed3aa2bfca2f14422d54f172676ec
Maïssane MAZOUZ <maissane.mazouz@gmail.com>
2022-07-25 12:02:57 +0200

    commit linux users

--- linux_users.sh
+++ linux_users.sh
@@ -2,13 +2,13 @@
 # Script to add a user to Linux system
 if [ $(id -u) -eq 0 ]; then
 	read -p "Enter username : " username
-	read -s -p "Enter password : " password
+	read -s -p "Enter password please : " password
 	egrep "^$username" /etc/passwd >/dev/null
 	if [ $? -eq 0 ]; then
 		echo "$username exists!"
 		exit 1
 	else
-		pass=$(perl -e 'print crypt($ARGV[0], "Enter password")' $password)
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
 		useradd -m -p $pass $username
 		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
 	fi
